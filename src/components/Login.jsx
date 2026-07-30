3853f8461ccf1f119447967275de6db5da1dc72c
rithi <rithikapillai8675@gmail.com>
2026-07-31 03:49:34 +0530

    debugLgoEr3

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -67,7 +67,7 @@ export default function Login({ onNavigate, setUser }) {
       {/* 1. Header with VIT Bhopal Logo */}
       <div className="page-header">
         <img 
-          src="public\images.jpg" 
+          src="images.jpg" 
           alt="VIT Bhopal Logo" 
           className="brand-logo" 
         />
