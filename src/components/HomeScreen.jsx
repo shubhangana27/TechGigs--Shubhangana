7b78000ec5dbc3ed0a43217c287ba61af73b2a7d
rithi <rithikapillai8675@gmail.com>
2026-07-31 03:47:07 +0530

    debugLogoEr

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -67,7 +67,7 @@ export default function Login({ onNavigate, setUser }) {
       {/* 1. Header with VIT Bhopal Logo */}
       <div className="page-header">
         <img 
-          src="src\images.jpg" 
+          src="public\images.jpg" 
           alt="VIT Bhopal Logo" 
           className="brand-logo" 
         />
